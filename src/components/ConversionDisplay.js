a813194e077113c0dfd7910ad3d34283158a2242
Fra <francescp.pierucci@icloud.com>
2025-01-29 23:51:09 +0100

    Primo commit

---
+++ src/components/ConversionDisplay.js
@@ -0,0 +1,21 @@
+import React,{useState} from "react";
+import Container from "react-bootstrap/esm/Container";
+import CurrencyDisplay from "./CurrencyDisplay";
+
+
+const ConversionDisplay = () =>{
+
+    return (
+
+<Container className="container-display " >
+<CurrencyDisplay />
+
+</Container>
+
+    )
+
+
+}
+
+
+export default ConversionDisplay
---
+++ src/utils/api.js
@@ -0,0 +1,5 @@
+
+
+const API_KEY = '7d3a9aee4b4559aab55edf9076dbc8cf'
+
+export  default API_KEY
